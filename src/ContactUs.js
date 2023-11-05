30a8e4d358809bdf9231e0960247a7fba774248e
McBay Balatbat <mcbaybalatbat5@gmail.com>
2023-11-06 01:50:43 +0800

    add emailjs sdk

---
+++ src/ContactUs.js
@@ -0,0 +1,38 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+export const ContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_1ga0xts",
+        "template_hd2ppb4",
+        form.current,
+        "wZUpMb9E0cfy_3DVt"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <form ref={form} className="contactForm" onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
